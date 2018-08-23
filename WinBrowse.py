2036063b68e0ce858e5720c38d79947a51c754b9
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-08-22 20:17:07 -0700

    Create WinBrowse.py

---
+++ WinBrowse.py
@@ -0,0 +1,10 @@
+from PyQt5.QtCore 
+import QUrl 
+from PyQt5.QtWidgets 
+import QApplication 
+from PyQt5.QtWebKitWidgets 
+import QWebView import sys
+app = QApplication(sys.argv)
+view = QWebView()
+view.show()
+view.setUrl(QUrl(“http://google.com”))
